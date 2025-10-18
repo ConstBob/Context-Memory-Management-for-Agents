3839db0e88b6b4a247e772762fe8db187c709f43
LKT123 <cartethyia@Lupa>
2025-10-17 20:41:17 -0700

    Add the discordBot

---
+++ discordBot.py
@@ -0,0 +1,53 @@
+# Packages
+import discord
+import datetime
+import os
+from dotenv import load_dotenv
+
+# Initialize the bot
+load_dotenv()
+TOKEN =  os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+
+bot = discord.Client(intents=intents)
+
+# Bot Behaviors
+@bot.event
+async def on_ready():
+    print(f"---Connected Successfully to the server---")
+
+@bot.event
+async def on_message(message: discord.Message):
+
+    if message.author == bot.user: # Ignore the message from bot itself 
+        return
+
+    if message.channel.name.lower() == "general":
+        if bot.user.mentioned_in(message): # Check if the bot was mentioned
+            sender = message.author
+            content = message.content.replace(bot.user.mention, "").strip()
+            time_sent = message.created_at
+
+            print(f"[{time_sent}] {sender.name}: {content}")
+            reply_text = get_agent_message(sender.name, content, time_sent)
+
+            await message.channel.send(
+                f"{sender.mention} {reply_text}"
+            )
+
+
+# ---Calling Functions---
+# Dock it with the API
+def get_agent_message(username : str, inquiry : str, timestamp: datetime.datetime) -> str:
+    # API Implemented here--------------------------------------------------------
+    
+    # ======================================
+    reply_text = inquiry  # Expected to be strings
+    #-----------------------------------------------------------------------------
+    return reply_text
+
+# Start the bot
+bot.run(TOKEN)
